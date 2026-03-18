d946015ebf2791ba3929889f00db6f6ae5905d78
unknown <nduflot@dawan.fr>
2026-03-18 12:05:40 +0100

    css formulaire partie 1 input, select, button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 
-
-
 function toggleHidden(cible){
     document.getElementById(cible).classList.toggle('hidden');
+}
+
+function getValue(element, cible){
+    document.querySelector(cible).innerText = element.value;
 }
